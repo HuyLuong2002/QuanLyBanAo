cf0ae5df287cb85a70f239ac11df9bd55a8b69f4
HuyLuong2002 <vienhuyluong2002@gmail.com>
2023-09-29 16:41:01 +0700

    first commit

---
+++ QuanLyBanAo-Backend/src/test/java/com/example/quanlybanaobackend/QuanLyBanAoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quanlybanaobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyBanAoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
