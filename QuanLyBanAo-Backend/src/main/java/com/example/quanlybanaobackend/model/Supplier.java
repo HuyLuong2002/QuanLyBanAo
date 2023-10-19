1325a7191edceb8511516c3f10c23ed423fb58e4
HuyLuong2002 <vienhuyluong2002@gmail.com>
2023-10-19 19:24:10 +0700

    commit

--- QuanLyBanAo-Backend/src/main/java/com/example/quanlybanaobackend/model/Supplier.java
+++ QuanLyBanAo-Backend/src/main/java/com/example/quanlybanaobackend/model/Supplier.java
@@ -17,40 +17,4 @@ public class Supplier {
     private int id;
     private String name;
     private boolean isDeleted;
-<<<<<<< HEAD
-=======
-
-    public Supplier() {
-    }
-
-    public Supplier(int id, String name, boolean isDeleted) {
-        this.id = id;
-        this.name = name;
-        this.isDeleted = isDeleted;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public boolean getDeleted() {
-        return isDeleted;
-    }
-
-    public void setDeleted(boolean deleted) {
-        isDeleted = deleted;
-    }
->>>>>>> 72728cedae41c235d679d87b391fda0fced1132a
 }
