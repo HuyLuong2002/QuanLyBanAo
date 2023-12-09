3ad8db2cb502bb7cc8ea147fed9286ecadd7f92d
HuyLuong2002 <vienhuyluong2002@gmail.com>
2023-12-09 12:35:32 +0700

    09/12/2023

--- QuanLyBanAo-Backend/src/main/java/com/example/quanlybanaobackend/controller/CartController.java
+++ QuanLyBanAo-Backend/src/main/java/com/example/quanlybanaobackend/controller/CartController.java
@@ -3,10 +3,10 @@ package com.example.quanlybanaobackend.controller;
 import com.example.quanlybanaobackend.constant.Constant;
 import com.example.quanlybanaobackend.dto.OrderInformationRequest;
 import com.example.quanlybanaobackend.model.*;
-import com.example.quanlybanaobackend.service.OrderService;
-import com.example.quanlybanaobackend.service.ProductService;
-import com.example.quanlybanaobackend.service.ShoppingCartService;
-import com.example.quanlybanaobackend.service.UserService;
+import com.example.quanlybanaobackend.service.*;
+import com.paypal.api.payments.Links;
+import com.paypal.api.payments.Payment;
+import com.paypal.base.rest.PayPalRESTException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -38,6 +38,12 @@ public class CartController {
 
     @Autowired
     private EmailController emailController;
+
+    @Autowired
+    PayPalService service;
+
+    public static final String SUCCESS_URL = "/cart/pay/success";
+    public static final String CANCEL_URL = "/cart/pay/cancel";
 
     @GetMapping(path = {"/getUserCart"})
     public ResponseEntity<Map<String, Object>> getUserCart() {
@@ -195,6 +201,95 @@ public class CartController {
             return new ResponseEntity<>(response, HttpStatus.OK);
         }
         response.put("message", "Bạn chưa đăng nhập");
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("/pay")
+    public ResponseEntity<Map<String, Object>> payment(@RequestBody OrderInformationRequest orderInformationRequest) throws PayPalRESTException {
+        Map<String, Object> response = new HashMap<>();
+        response.put("success", false);
+        if (authController.getUserLogin() != null) {
+            String username = SecurityContextHolder.getContext().getAuthentication().getName();
+            User user = userService.findByUsername(username);
+            if (user.getTel().trim().isEmpty() || user.getAddress().trim().isEmpty()) {
+                response.put("message", "Bạn chưa nhập số điện thoại và địa chỉ");
+                return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+            } else {
+                ShoppingCart cart = user.getShoppingCart();
+                Order order = new Order();
+                List<OrderDetail> orderDetailList = new ArrayList<>();
+                order.setOrderDate(new Date());
+                order.setTotalQuantity(cart.getTotalItems());
+                order.setTotalPrice(cart.getTotalPrices() + order.getShippingFee());
+                order.setOrderStatus(Constant.OrderStatus.ACTIVE);
+                order.setShipStatus(Constant.ShipStatus.APPROVAL);
+                order.setPaymentStatus(Constant.PaymentStatus.UNPAID);
+                order.setNotes(orderInformationRequest.getNotes());
+                order.setUser(user);
+                if (orderInformationRequest.getPaymentMethod().name().equals("CASH")) {
+                    order.setPaymentMethod(Constant.PaymentMethod.CASH);
+                } else if (orderInformationRequest.getPaymentMethod().name().equals("BANKING")) {
+                    order.setPaymentMethod(Constant.PaymentMethod.BANKING);
+                }
+                for (CartItem item :
+                        cart.getCartItem()) {
+                    OrderDetail orderDetail = new OrderDetail();
+                    orderDetail.setOrder(order);
+                    orderDetail.setProduct(item.getProduct());
+                    orderDetail.setQuantity(item.getQuantity());
+                    orderDetail.setUnitPrice(item.getProduct().getPrice());
+                    orderDetailList.add(orderDetail);
+                }
+                order.setOrderDetails(orderDetailList);
+                Payment payment = service.createPayment(order.getTotalPrice(), "USD", "PAYPAL",
+                        "ORDER", order.getNotes(), "http://localhost:8081/api/v1/" + CANCEL_URL,
+                        "http://localhost:8081/api/v1" + SUCCESS_URL);
+                for (Links link : payment.getLinks()) {
+                    if (link.getRel().equals("approval_url")) {
+                        response.put("approval_link", link.getHref());
+                    }
+                }
+                orderService.save(order);
+                shoppingCartService.delete(cart);
+                response.put("success", true);
+                response.put("message", "Đặt đơn hàng thành công");
+                //Send mail to customer
+                EmailDetails emailDetails = new EmailDetails();
+                emailDetails.setRecipient(user.getEmail());
+                emailDetails.setSubject("Thông báo đơn hàng");
+                emailDetails.setMsgBody("Cảm ơn bạn đã đặt hàng ở công ty chúng tôi. " +
+                        "Chúng tôi xin thông báo với bạn về đơn hàng của bạn đã đặt thành công");
+                String result = emailController.sendMail(emailDetails);
+                System.out.println(result);
+                return new ResponseEntity<>(response, HttpStatus.OK);
+            }
+        }
+        response.put("message", "Bạn chưa đăng nhập");
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping(value = CANCEL_URL)
+    public ResponseEntity<Map<String, Object>> cancelPay() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("success", false);
+        response.put("message", "Thanh toán thất bại");
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping(value = SUCCESS_URL)
+    public ResponseEntity<Map<String, Object>> successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId) {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            response.put("success", false);
+            Payment payment = service.executePayment(paymentId, payerId);
+            if (payment.getState().equals("approved")) {
+                response.put("success", true);
+                response.put("response", payment.toJSON());
+                return new ResponseEntity<>(response, HttpStatus.OK);
+            }
+        } catch (PayPalRESTException e) {
+            System.out.println(e.getMessage());
+        }
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
